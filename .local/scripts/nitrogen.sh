46cf3e2d1ee85de8df9a4ce75487bdf6a0137565
otaviocosta2110 <otaviocosta23212@gmail.com>
2024-07-28 21:06:47 -0300

    scripts

---
+++ .local/scripts/nitrogen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# nitrogen --set-zoom-fill /home/otavio/Pictures/reicity.png --head=0
+# nitrogen --set-zoom-fill /home/otavio/Pictures/reicity.png --head=1
+nitrogen --set-zoom-fill /home/otavio/Pictures/000000.png --head=0
+nitrogen --set-zoom-fill /home/otavio/Pictures/000000.png --head=1
