e2e9b5ec85839885f8e4a961503e36f3637b50ae
Otávio Costa <otaviocosta23212@gmail.com>
2025-05-22 13:20:21 -0300

    add some dirs in push

--- .local/scripts/nitrogen.sh
+++ .local/scripts/nitrogen.sh
@@ -9,7 +9,9 @@ for i in $(seq 0 1); do
     # nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/reiblk.png --head=$i
     # nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/rei_blue.png --head=$i
     # nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/rei_look.png --head=$i
-    nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/lain_black.png --head=$i
+    # nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/lain_black.png --head=$i
+    nitrogen --set-zoom-fill /home/otavio/Pictures/wallpapers/2220.jpg --head=$i
+
     # nitrogen --set-zoom-fill /home/otavio/Imagens/Wallpapers/eva-blue.png --head=$i
 done
 # nitrogen --set-zoom-fill /home/otavio/Pictures/reicity.png --head=0
